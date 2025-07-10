a7cbd04990c5a5477a353c7ad800aa3d591ac5fc
Joseph Akayesi <joseph.akayesi@amalitech.com>
2025-07-10 11:47:52 +0000

    [feat]: add uptime to api

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -25,9 +26,11 @@ func (t *TemplateEngine) Load() error {
 
 
 func main() {
+		startTime := time.Now()
+
 	// Parse all templates in the views folder
 	tmpl := template.Must(template.ParseGlob("views/*.html"))
-	version := "0.0.2"
+	version := "0.0.3"
 
     app := fiber.New(fiber.Config{
         Views: &TemplateEngine{
@@ -41,11 +44,16 @@ func main() {
         })
     })
 
+	
+
 	app.Get("/api", func(c *fiber.Ctx) error {
+		uptime := time.Since(startTime).Round(time.Second) // rounded for readability
+		
 		return c.JSON(fiber.Map{
 			"success": true,
 			"version": fmt.Sprintf("v%s", version),
 			"message": "server up and running",
+			"uptime":  uptime.String(),
 		})
 	})
 
